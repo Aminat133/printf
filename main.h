8320fe08877e55039a1ce07966fc4801ea826896
Armand75 <isubiza56@gmail.com>
2023-09-19 16:13:27 -0700

    couple of prototypes

--- main.h
+++ main.h
@@ -7,6 +7,8 @@ int _printf(const char *format, ...);
 int _print_string(va_list args);
 int _print_char(va_list args);
 int _putchar(char c);
+int _print_int_helper(int n);
+int _print_int(va_list args);
 #endif
 
 
